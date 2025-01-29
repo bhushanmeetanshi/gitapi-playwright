b0bc4d7a23c9f4800950aed7d13253333c08fb9b
Bhushan Trivedi <bhushan.trivedi@meetanshi.com>
2025-01-29 18:56:28 +0530

    new commit

--- test/gitHubAPI.spec.ts
+++ test/gitHubAPI.spec.ts
@@ -1,4 +1,5 @@
 import { test, expect } from "@playwright/test";
+import { log } from "console";
 import * as pw from "playwright";
 
 const url = process.env.URL;
@@ -10,10 +11,10 @@ test.describe("GIT API Testing", () => {
   test("Create Issue", async () => {
     const requestContext = await pw.request.newContext();
     const response = await requestContext.post(
-      `${url}/${owner}/${repo}/issues`,
+      `https://api.github.com/repos/${owner}/${repo}/issues`,
       {
         data: {
-          title: "Found a bug",
+          title: "Found a bug12",
           body: "I'm having a problem with this.",
           labels: ["bug"],
         },
@@ -25,23 +26,26 @@ test.describe("GIT API Testing", () => {
     );
     expect(response.status()).toBe(201);
   });
-  test("Create Repository", async () => {
+  test("Create repository", async () => {
     const requestContext = await pw.request.newContext();
-    const response = await requestContext.post(`${url}/user/repos`, {
-      data: {
-        name: "private_repo4",
-        description: "This is your first repository",
-        homepage: "https://github.com",
-        private: true,
-        has_issues: true,
-        has_projects: true,
-        has_wiki: true,
-      },
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${token}`, // If authentication is needed
-      },
-    });
+    const response = await requestContext.post(
+      `https://api.github.com/user/repos`,
+      {
+        data: {
+          name: "private_repo___1",
+          description: "This is your first repository",
+          homepage: "https://github.com",
+          private: true,
+          has_issues: true,
+          has_projects: true,
+          has_wiki: true,
+        },
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
     expect(response.status()).toBe(201);
   });
 });
